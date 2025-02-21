a9abc8190cba19bf60c3146a818c5d4a458141e2
synqspz <synqspz@gmail.com>
2025-02-21 17:24:59 +0100

    [Functional] Add type constructor

---
+++ Functional/HigherKindedTypes/ITypeConstructor.cs
@@ -0,0 +1,8 @@
+namespace Functional.HigherKindedTypes;
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="TC"></typeparam>
+/// <typeparam name="T"></typeparam>
+public interface ITypeConstructor<TC, T>;
