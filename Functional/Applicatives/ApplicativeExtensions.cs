9e1c61d2aec348ac5894678cbca16655b47a0d3c
synqspz <synqspz@gmail.com>
2025-02-21 17:25:34 +0100

    [Functional] Add applicative

---
+++ Functional/Applicatives/ApplicativeExtensions.cs
@@ -0,0 +1,75 @@
+using Functional.HigherKindedTypes;
+
+namespace Functional.Applicatives;
+
+/// <summary>
+/// 
+/// </summary>
+public static class ApplicativeExtensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <param name="selector"></param>
+    /// <typeparam name="TC"></typeparam>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <returns></returns>
+    public static ITypeConstructor<TC, T3> Lift<TC, T1, T2, T3>(
+        this ITypeConstructor<TC, T1> left, 
+        ITypeConstructor<TC, T2> right, 
+        Func<T1, T2, T3> selector)
+        where TC : IApplicative<TC>
+        => TC.Lift(left, right, selector);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="f"></param>
+    /// <param name="input"></param>
+    /// <typeparam name="TC"></typeparam>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <returns></returns>
+    public static ITypeConstructor<TC, T2> Apply<TC, T1, T2>(
+        this ITypeConstructor<TC, Func<T1, T2>> f,
+        ITypeConstructor<TC, T1> input)
+        where TC : IApplicative<TC>
+        => f.Sequence()(input);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="f"></param>
+    /// <typeparam name="TC"></typeparam>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <returns></returns>
+    public static ITypeConstructor<TC, T2> Apply<TC, T1, T2>(
+        this ITypeConstructor<TC, T1> input,
+        ITypeConstructor<TC, Func<T1, T2>> f)
+        where TC : IApplicative<TC>
+        => f.Apply(input);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="f"></param>
+    /// <typeparam name="TC"></typeparam>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <returns></returns>
+    public static Func<ITypeConstructor<TC, T1>, ITypeConstructor<TC, T2>> 
+        Sequence<TC, T1, T2>(
+            this ITypeConstructor<TC, Func<T1, T2>> f)
+        where TC : IApplicative<TC>
+        => input
+            => Lift(
+                f, 
+                input, 
+                (g, t1) => g(t1));
+}
