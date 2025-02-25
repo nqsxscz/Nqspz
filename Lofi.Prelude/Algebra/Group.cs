697433598c5c91fbbfa0f49e25543ecbf6e5d563
synqspz <synqspz@gmail.com>
2025-02-25 13:24:48 +0100

    [Prelude][Algebra] Add power operation for multiplicative group

--- Lofi.Prelude/Algebra/Group.cs
+++ Lofi.Prelude/Algebra/Group.cs
@@ -1,6 +1,7 @@
 using Lofi.Prelude.Algebra.Trait;
 using Lofi.Prelude.Algebra.Trait.Additive;
 using Lofi.Prelude.Algebra.Trait.Multiplicative;
+using Lofi.Prelude.Numeric.Trait;
 
 namespace Lofi.Prelude.Algebra;
 
@@ -21,4 +22,23 @@ public static class Group
     public static T Divide<T>(this T left, T right)
         where T : notnull, IMultiplicativeGroup<T>
         => left / right;
+
+    public static T Power<T, TNatural>(
+        this T left,
+        TNatural right)
+        where T : IMultiplicativeGroup<T>
+        where TNatural : INatural<TNatural>
+        => right >= TNatural.Zero ? 
+            left.Power1(right) 
+            : 
+            left.Power1(right)
+                .Invert();
+    
+    private static T Power1<T, TNatural>(
+        this T left, 
+        TNatural right)
+        where T : IMultiplicativeMonoid<T>
+        where TNatural : INatural<TNatural>
+        => right.Equals(TNatural.Zero) ? 
+            T.One : left * left.Power1(right - TNatural.One);
 }
