e901601990d6a4f93f34f69117a23053e5754a41
synqspz <synqspz@gmail.com>
2025-02-27 21:52:18 +0100

    [Lang][Stochastic][Sample] Add runge-kutta sampler

---
+++ Lofi.Lang/Stochastic/Sample/Type/IRungeKuttaSampler.cs
@@ -0,0 +1,84 @@
+using Lofi.Lang.Stochastic.Process.Continuous;
+using Lofi.Lang.Stochastic.Process.Continuous.Instance.Type.Ito;
+using Lofi.Lang.Stochastic.Trajectory;
+using Lofi.Lang.Stochastic.Trajectory.Type;
+using Lofi.Prelude.Algebra;
+using Lofi.Prelude.Data.Instance.Seq.Type;
+using Lofi.Prelude.Numeric;
+using Lofi.Prelude.Numeric.Trait;
+
+namespace Lofi.Lang.Stochastic.Sample.Type;
+
+using Scannable = Prelude.Control.Scannable;
+
+public interface IRungeKuttaSampler
+    : ISampler
+{
+    ITrajectory<T> ISampler.Sample<T>(
+        IItoStochasticProcess<T> process, 
+        ISeq<DateTime> times,
+        int seed)
+    {
+        var dt = 
+            StochasticProcess
+                .Time
+                .Differentiate(process.Converter)
+                .Sample(this, times, seed);
+        
+        var dwt =
+            process
+                .Wiener
+                .Differentiate()
+                .Sample(this, times, seed);
+
+        var at =
+            process
+                .Left
+                .Sample(this, times, seed);
+        
+        var bt =
+            process
+                .Right
+                .Sample(this, times, seed);
+
+        return Scannable
+            .ScanLeft(
+                dt, 
+                dwt, 
+                at, 
+                bt, 
+                process.Init, 
+                (su, du, dwu, au, bu) => 
+                    Next(
+                        process.Drift, 
+                        process.Volatility, 
+                        su, 
+                        du, 
+                        dwu, 
+                        au, 
+                        bu))
+            .ToTrajectory();
+    }
+
+    private static T Next<T>(
+        Func<T, T, T> drift,
+        Func<T, T, T> volatility,
+        T su,
+        T du,
+        T dwu,
+        T au,
+        T bu)
+        where T : IReal<T>
+    {
+        var sbaru = 
+            su 
+            + drift(su, au) * du 
+            + volatility(su, bu) * du.Sqrt();
+        var next =
+            su
+            + drift(su, au) * du
+            + volatility(su, bu) * dwu
+            + T.Two.Invert() * (drift(sbaru, au) - drift(su, au)) * (dwu.Square() - du) / du.Sqrt();
+        return next;
+    }
+}
